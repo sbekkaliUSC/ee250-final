6e9c5fc5ec7b72614e43b62d7411a8f043b1c8e4
Saleem Bekkali <bekkali@usc.edu>
2021-12-03 23:28:38 -0800

    Set up queue

--- ee250/vm_mqtt.py
+++ ee250/vm_mqtt.py
@@ -6,9 +6,17 @@ Repo: https://github.com/sbekkaliUSC/ee250-final
 import sys
 import paho.mqtt.client as mqtt
 import time
+from collections import deque
+import threading
+lock = threading.Lock()
 
 def custom_callback_UR(client, userdata, message):
-    print(message.payload.decode('utf-8', 'strict'))
+    msg = message.payload.decode('utf-8', 'strict')
+    print(msg)
+    with lock:
+        if len(queue) == 60:
+            queue.popleft() # removes oldest boolean in queue to free up space
+        queue.append(msg == "1") # if within UR range put True else put False
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
@@ -19,12 +27,23 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload, "utf-8"))
 
+def compute_objects_minute(queue):
+    total_detected = 0
+    object_present = False
+    for i in queue:
+        if not object_present and i: # true if object first detected
+            total_detected += 1
+        object_present = i
+    return total_detected
+
+
 if __name__ == '__main__':
     try:
         if (len(sys.argv) >= 2):
             threshold = int(sys.argv[1])
             if threshold < 0 or threshold > 517:
                 print("Warning: Threshold values outside of range [0, 517] are unreliable")
+        queue = deque([])
         client = mqtt.Client()
         client.on_message = on_message
         client.on_connect = on_connect
@@ -34,7 +53,10 @@ if __name__ == '__main__':
             client.publish("bekkali/threshold", threshold)
 
         while True:
-            time.sleep(1)
+            time.sleep(10)
+            with lock:
+                print("Objects detected:%d", compute_objects_minute(queue))
+                print(queue)
     except ValueError:
         print("Specified Threshold value must be an integer formatted as \"/python3 vm_mqtt.py [threshold]\".")
 
