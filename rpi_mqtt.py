488835151a494f9eba14a9fc1d3526197d71d38e
Saleem Bekkali <bekkali@usc.edu>
2021-12-03 19:36:06 -0800

    Write test UR and LED for RPI

---
+++ rpi_mqtt.py
@@ -0,0 +1,58 @@
+""" EE 250 Final Project
+Team members: Saleem Bekkali
+Repo: https://github.com/sbekkaliUSC/ee250-final
+Source: Reusing parts of code from grovepi_sensors.py and rpi_pub_and_sub.py
+from Lab 2 and 5, respectively.
+"""
+
+"""python3 interpreters in Ubuntu (and other linux distros) will look in a 
+default set of directories for modules when a program tries to `import` one. 
+Examples of some default directories are (but not limited to):
+  /usr/lib/python3.5
+  /usr/local/lib/python3.5/dist-packages
+
+The `sys` module, however, is a builtin that is written in and compiled in C for
+performance. Because of this, you will not find this in the default directories.
+"""
+import sys
+import time
+import paho.mqtt.client as mqtt
+
+# By appending the folder of all the GrovePi libraries to the system path here,
+# we are successfully `import grovepi`
+sys.path.append('../../Software/Python/')
+# This append is to support importing the LCD library.
+sys.path.append('../../Software/Python/grove_rgb_lcd')
+
+import grovepi
+from grove_rgb_lcd import *
+
+if __name__ == '__main__':
+
+    led = 3 # D3
+    ultrasonic = 4    # D4
+
+    client = mqtt.Client()
+    client.on_message = on_message
+    client.on_connect = on_connect
+    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+    client.loop_start()
+
+    while True:
+        try:
+            #sleep 1 second
+            time.sleep(1)
+
+            distance = grovepi.ultrasonicRead(ultrasonic) # measures distance via ultrasonic sensor
+            threshold = 50 # TEMPORARY TEST VALUE
+
+            if (distance <= threshold):
+                #client.publish("bekkali/ultrasonicRanger", 1)
+                digitalWrite(led, 1)
+            else:
+                #client.publish("bekkali/ultrasonicRanger", 0)
+                digitalWrite(led, 0)
+
+
+        except (IOError,TypeError) as e:
+            print("Error")
